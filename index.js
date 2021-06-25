042fc42a7001d2be1c0ceb9819d72c5d434a914d
sameghosts <seanfeiner.brown@gmail.com>
2021-06-25 04:33:37 -0400

    server psuedo index

---
+++ index.js
@@ -0,0 +1,16 @@
+//TODO: Install base dependencies and requirements
+
+
+//TODO: Set up GQL requirements
+
+//TODO Instantiate App and structure
+
+// middleware
+
+//TODO: GraphQL Apollo server
+
+
+// Dummy API Home Route
+
+// App listen
+
