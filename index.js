c3b12cca7943124b92d7c2ea71d70db2ac187db3
sameghosts <seanfeiner.brown@gmail.com>
2021-06-26 17:16:40 -0400

    graphql apollo set up

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const EXPRESS = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 //TODO: Set up GQL requirements
-// const {
-//   typeDefs,
-//   resolvers
-// } = require('./graphQL');
-// const {
-//   schemaDirectives
-// } = require('./graphQL/directives');
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
 
 //TODO Instantiate App and structure
 const app = EXPRESS();
@@ -18,33 +23,23 @@ const app = EXPRESS();
 // Models for graphql server context
 // const AppModels = require('./models');
 // middleware
-  //Add middleware after build
+//Add middleware after build
 
 //TODO: GraphQL Apollo server
-// const SERVER = new ApolloServer({
-//   typeDefs: typeDefs,
-//   resolvers: resolvers,
-//   //TODO: Check and add in directives
-//   // schemaDirectives: schemaDirectives,
-//   playground: true,
-//   //TODO: check and add in context
-//   // context: ({ req }) => {
-//   //   let {
-//   //     user,
-//   //     isAuth,
-//   //   } = req;
-//   //   return{
-//   //     req,
-//   //     user,
-//   //     isAuth,
-//   //     ...AppModels,
-//   //   }
-//   // }
-// });
+const SERVER = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  //TODO: Check and add in directives
+  // schemaDirectives: schemaDirectives,
+  playground: true,
+  //TODO: check and add in context
+  context: {}
 
-// SERVER.applyMiddleware({
-//   app: app
-// })
+});
+
+SERVER.applyMiddleware({
+  app: app
+})
 
 
 // Dummy API Home Route
@@ -53,6 +48,6 @@ app.get('/', (req, res) => {
   res.send("JobJournal API portal home")
 })
 // App listen
-app.listen(process.env.PORT || 3001, () =>{
+app.listen(process.env.PORT || 3001, () => {
   console.log(`✅ You are listening to to smooth sounds of Port ${process.env.PORT || 3001}`)
 })
