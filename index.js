5f76beb4bef02571355c7427508b6756a2b616fa
sameghosts <seanfeiner.brown@gmail.com>
2021-06-26 16:48:33 -0400

    server stubb removed context

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 //TODO: Install base dependencies and requirements
-
+require('dotenv').config();
+const EXPRESS = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 //TODO: Set up GQL requirements
+const {
+  typeDefs,
+  resolvers
+} = require('./graphQL');
+const {
+  schemaDirectives
+} = require('./graphQL/directives');
 
 //TODO Instantiate App and structure
+const app = EXPRESS();
 
+// Models for graphql server context
+const AppModels = require('./models');
 // middleware
+  //Add middleware after build
 
 //TODO: GraphQL Apollo server
+const SERVER = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  schemaDirectives: schemaDirectives,
+  playground: true,
+  context: ({ req }) => {
+    let {
+      user,
+      isAuth,
+    } = req;
+    return{
+      req,
+      user,
+      isAuth,
+      ...AppModels,
+    }
+  }
+});
 
 
 // Dummy API Home Route
