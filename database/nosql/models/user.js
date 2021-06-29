3f563e2445af1734494458872dbefa1e341df7dd
sameghosts <seanfeiner.brown@gmail.com>
2021-06-29 16:06:02 -0400

    userSchema

--- database/nosql/models/user.js
+++ database/nosql/models/user.js
@@ -1,6 +1,7 @@
 // Dependencies
 
 const mongoose =require('mongoose');
+const { NUMBER } = require('sequelize/types');
 
 // Instantiate Schema
 const Schema = mongoose.Schema;
@@ -9,13 +10,72 @@ const options = {
   timestamps: true
 }
 
+//education subschema
+const userSchoolsSchema = new this.schema({
+  schoolName: String,
+  degree: String,
+  gradYear: Number,
+  honorsAwards: String
+})
+
+
 //Profile SubSchema
-const userProfile = new Schema({
+const userProfileSchema = new Schema({
+  userLocation: {
+    locationCity: String,
+    locationState: String,
+    locationAddress: String,
+    locationZip: Number
+  },
+  userIdentity: {
+    jobTitle: String,
+    jobType: String,
+    jobLevel: String,
+    hardSkills: [String],
+    softSkills: [String],
+    companyCulture: String,
+    techStack: [String],
+    employmentType: String,
+    payRange: String,
+    notes: String
+  },
+  bio: String,
+  education: [userSchoolsSchema]
 
 });
 //User Schema
 const userSchema = newSchema({
-
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  }, 
+  password: {
+    type: String,
+    required: true
+  }, 
+  profile: [userProfileSchema],
+  jobJournal: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'JobJournal'
+  }],
+  jobCollection: [{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'JobCollection',
+  }]
 }, options)
 
 // exports
